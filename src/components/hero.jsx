d045277a1b4e4188fb6722d01c38d491455c2f3d
Sanskar Sijariya <sanskarsijariya80@gmail.com>
2024-10-12 23:19:00 +0530

    Uploding Brand Page frontend Project Learning React Js

---
+++ src/components/hero.jsx
@@ -0,0 +1,25 @@
+const HeroSection=()=>{
+    return(
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE EBST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.
+                   YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.
+                </p>
+               
+                <div className="hero-btn">
+                    <button>Shop Now</button>
+                    <button className="secondary">Category</button>
+                </div>
+                <div className="shopping">
+                    <p> Also Available on Amazon & Flipkart</p>
+                </div>
+                </div>
+                <div className="hero-image">
+                    <img src="https://rukminim1.flixcart.com/image/1664/1664/shoe/z/y/r/404-621077-nike-9-original-imadvguhafmerw44.jpeg?q=90" alt="shoes"/>
+                </div>
+           
+        </main>
+    );
+}
+export default HeroSection
---
+++ src/components/navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation=()=>{
+    return (
+    <nav className="container">
+        <div className="logo">
+          <img src="https://logos-world.net/wp-content/uploads/2020/04/Nike-Logo-1978-present.jpg" alt='logo' />
+        </div>
+        <ul>
+          <li href="#"> Menu</li>
+          <li href="#" >Location</li>
+          <li href="#" >About</li>
+          <li href="#" >Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    );
+}
+export default Navigation;
